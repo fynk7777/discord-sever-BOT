21c16f7462ac0047859c80023b3281d4bb0fe411
fynk7777 <fynk7777@yahoo.co.jp>
2024-07-04 20:23:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -332,7 +332,7 @@ async def handle_bump_notification(message):
     await message.channel.send(embed=notice_embed)
 
 async def send_update_message():
-    update_id = 1235075709026697249
+    update_id = 1236087834352291863
     user_id = 1212687868603007067  # bakabonnpapa のユーザーID を設定する
     user = await bot.fetch_user(user_id)
     update = await bot.fetch_channel(update_id)
